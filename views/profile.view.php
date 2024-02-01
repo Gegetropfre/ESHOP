427d5455eccfc1c83256c81e8b763c8efdc28f2b
Gegetropfre <hete.audric@gmail.com>
2024-02-01 15:26:11 +0100

    :sparkles: profile page

--- views/profile.view.php
+++ views/profile.view.php
@@ -6,7 +6,7 @@ session_start(); ?>
     <h1><?= $_SESSION['user']['name'] ?></h1>
     <h2>email : <?= $_SESSION['user']['email'] ?></h2>
 
-
+    <img src="../uploads/avatar/doigby.jpg" id="profile-avatar" alt="">
 </div>
 
 
